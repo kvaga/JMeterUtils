49180ea36fb8be802fb8bc955b89213b60a7218b
kvaga <32394407+kvaga@users.noreply.github.com>
2018-06-08 14:10:14 +0300

    JSON parsing of appointments was finished
    Funtionaluty:
    you can shoose a specific day, start and end time, specify random or earliest time periods

--- JMeterUtils/src/Exec.java
+++ JMeterUtils/src/Exec.java
@@ -2,8 +2,10 @@ import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -44,10 +46,14 @@ public class Exec {
 	
 	static void scenario(EMIASScheduleResponse response) throws ParseException {
 		SimpleDateFormat sdfDay = new SimpleDateFormat("yyyy-MM-dd");
-		SimpleDateFormat sdfDayTime = new SimpleDateFormat("yyy-MM-dd HH:mm:ss");
+		SimpleDateFormat sdfDayTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		
 		String strDate="2018-06-15";
-		String strStartTime="19:13:00";
-		String strEndTime="21:00:00";
+		String strStartTime="10:10:00";
+		String strEndTime="11:30:00:00";
+		
+		// If true then earliest else random
+		boolean requireEarliest=true;
 		
 		Date date = sdfDay.parse(strDate);
 		Date startTime = sdfDayTime.parse(strDate + " " + strStartTime);
@@ -57,20 +63,60 @@ public class Exec {
 		System.out.println("Date: " + date);
 		System.out.println("StartTime: " + startTime);
 		System.out.println("EndTime" + endTime);
+		
+		ArrayList<EMIASTimePeriod> suiteableTimePeriods = new ArrayList<EMIASTimePeriod>();
+		
 		// Finding required date
 		for(EMIASSchedule schedule : response.getResult().getSchedules()) {
 			if(schedule.getDate().equals(date)) {
 				System.out.println("Дата обнаружена: " + date);
 				// Finding required time period
 				for(EMIASTimePeriod timePeriod : schedule.getResourceSchedule().getTimePeriods()) {
-//					if()) {
-//						
-//					}
+					if(
+							(
+							timePeriod.getAllowedAppointment()
+							&& (timePeriod.getStartTime().after(startTime)	|| timePeriod.getStartTime().equals(startTime))) 
+							&& (timePeriod.getEndTime().before(endTime) 	|| timePeriod.getEndTime().equals(endTime))
+							) {
+						/*
+						System.out.println("Найдено подходящее коно посещения: \n"
+								+ "startTime: " + timePeriod.getStartTime() + "\n" 
+								+ "endTime: " + timePeriod.getEndTime() + "\n" 
+								);
+						*/
+						suiteableTimePeriods.add(timePeriod);
+					}
 				}
 			}
 		}
+		System.out.println("Rол-во найденных доступных окон: " + suiteableTimePeriods.size());
+		
+		EMIASTimePeriod finalTimePeriod=null;
+		if(suiteableTimePeriods.size()>0) {
+			if(requireEarliest ) {
+				Date earliestStartTime = endTime;
+				for(EMIASTimePeriod tp : suiteableTimePeriods) {
+					if(tp.getStartTime().before(earliestStartTime) ) {
+						finalTimePeriod=tp;
+						earliestStartTime=tp.getStartTime();
+					}
+				}
+				System.out.println("Earliest time period: \n"
+						+ "startTime: " + finalTimePeriod.getStartTime() +"\n"
+						+ "endTime: " + finalTimePeriod.getEndTime() + "\n"
+						);
+			}else {
+				Random rand = new Random();
+				finalTimePeriod = suiteableTimePeriods.get(rand.nextInt(suiteableTimePeriods.size()));
+				
+				System.out.println("Random time period: \n"
+						+ "startTime: " + finalTimePeriod.getStartTime() +"\n"
+						+ "endTime: " + finalTimePeriod.getEndTime() + "\n"
+						);
+			}
+		}else {
+			System.err.println("Свободных окон не найдено");
+		}
 		
 	}
-	
-
 }
